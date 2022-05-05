252e398f66059ce0aa69855723193a617f6be2fc
Larry Borsato <larry@larryborsato.com>
2022-05-05 08:02:45 -0400

    DEV-2705 Add missing files.

---
+++ tests/unit/views/tasks-view.spec.ts
@@ -0,0 +1,27 @@
+import { shallowMount } from '@vue/test-utils'
+import TasksView from '../../../src/views/tasks-view.vue'
+
+describe('tasks-view.vue', () => {
+    let wrapper: any;
+
+    beforeEach(() => {
+        wrapper = shallowMount(TasksView, {
+            propsData: {},
+            config: {},
+            mocks: {},
+            stubs: {},
+        });
+    });
+
+    afterEach(() => {
+    });
+
+    test('can complete a task', async () => {
+        expect(wrapper.findComponent('[data-testid="tasks-complete-checkbox"]').exists())
+    })
+
+    test('can you show tasks view', async () => {
+        wrapper.vm.showHomeView()
+        expect(wrapper.showTasksView).toBe(true)
+    })
+})
